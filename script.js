0159dcbb3f0e72dcd66802624a70947f705e8e4b
sc0rpi-1 <147271936+sc0rpi-1@users.noreply.github.com>
2024-09-11 21:49:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  // Add zoom-in effect after page load
+  setTimeout(() => {
+    document.querySelector(".moon-background").classList.add("zoom-in");
+  }, 1000); // Start zoom 1 second after the page loads
+};
